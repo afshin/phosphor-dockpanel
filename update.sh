d204292252b99dbe1cfa2c8cf29610035ed28786
A. Darian <git@darian.af>
2015-12-01 14:33:58 +0000

    simplifying gh pages

--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
 cp ../phosphor-dockpanel/example/dashboard-bundle.js ./
-cp ../phosphor-dockpanel/example/dashboard.css ./
 cp ../phosphor-dockpanel/example/dashboard.html ./index.html
 cp ../phosphor-dockpanel/example/plots/*.js ./plots/
