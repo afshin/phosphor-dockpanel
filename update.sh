8cf2ce74f4e1ddf16527a379649c2b369c011acd
A. Darian <git@darian.af>
2015-12-08 16:58:29 +0000

    allow page to scroll

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 cp ../phosphor-dockpanel/example/dashboard-bundle.js ./
 cp ../phosphor-dockpanel/example/dashboard.html ./index.html
 cp ../phosphor-dockpanel/example/plots/*.js ./plots/
+cp ../phosphor-dockpanel/example/images/* ./images/
