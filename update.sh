a950b8d784d671bfde98f764d5bdf9122d69707c
A. Darian <git@darian.af>
2015-12-11 16:57:44 +0000

    redirect

--- update.sh
+++
@@ -1,4 +0,0 @@
-cp ../phosphor-dockpanel/example/dashboard-bundle.js ./
-cp ../phosphor-dockpanel/example/dashboard.html ./index.html
-cp ../phosphor-dockpanel/example/plots/*.js ./plots/
-cp ../phosphor-dockpanel/example/images/* ./images/
